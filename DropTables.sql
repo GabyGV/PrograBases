4e6381f7005a068ccec0bebdce89e3ee469e9efb
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2021-09-09 01:17:43 -0600

    Tablas

---
+++ DropTables.sql
@@ -0,0 +1,14 @@
+USE [PrograBases]
+
+DROP TABLE Usuario
+DROP TABLE Usuarios_Ver
+
+DROP TABLE Beneficiario_Cuenta
+DROP TABLE Beneficiario
+DROP TABLE Cuenta
+DROP TABLE Persona
+DROP TABLE TipoCuentaAhorro
+DROP TABLE Parentezcos
+DROP TABLE Tipo_Moneda
+DROP TABLE TipoDocIdentidad
+
--- Tables.sql
+++ Tables.sql
@@ -2,46 +2,99 @@ USE [PrograBases]
 
 CREATE TABLE TipoDocIdentidad
 (
-  ID_TDoc INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  ID_TDoc INT PRIMARY KEY NOT NULL,
   Tipo VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE Persona
+CREATE TABLE Tipo_Moneda
 (
-  ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  Nombre VARCHAR(64) NOT NULL,
-  ValorDocIdentidad INT NOT NULL
+  ID_TMoneda INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(16)
+);
+
+CREATE TABLE Parentezcos
+(
+  ID_Parentezco INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(16)
 );
 
 CREATE TABLE TipoCuentaAhorro
 (
-  ID_TCuenta INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  Nombre VARCHAR(64) NOT NULL,
-  TasaInteres INT NOT NULL,
-  Multa INT NOT NULL
+  ID_TCuenta INT PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(32) NOT NULL,
+  SaldoMinimo MONEY NOT NULL,
+  MultaSaldoMinimo MONEY NOT NULL,
+  NumRetiros_Humanos INT NOT NULL,
+  NumRetiros_Automaticos INT NOT NULL,
+  ComisionHumano MONEY NOT NULL,
+  ComisionAutomatico MONEY NOT NULL,
+  Interes INT NOT NULL,
+
+  ID_TMoneda INT NOT NULL,
+  FOREIGN KEY (ID_TMoneda) REFERENCES Tipo_Moneda(ID_TMoneda)
 );
 
-CREATE TABLE CuentaAhorro
+CREATE TABLE Persona
 (
-  ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  Saldo INT NOT NULL,
-  FechaCreacion DATE NOT NULL DEFAULT GETDATE()
+  ID_Persona INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(64) NOT NULL,
+  ValorDocIdentidad VARCHAR(16) NOT NULL,
+  FechaNacimiento DATE NOT NULL,
+  Email VARCHAR(64) NOT NULL,
+  Telefono1 VARCHAR(16) NOT NULL,
+  Telefono2 VARCHAR(16) NOT NULL,
+
+  ID_TDoc INT NOT NULL,
+  FOREIGN KEY (ID_TDoc) REFERENCES TipoDocIdentidad(ID_TDoc)
+);
+
+CREATE TABLE Cuenta
+(
+  ID_Cuenta INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  ValorDocIdentidad VARCHAR(16) NOT NULL,
+  NumeroCuenta VARCHAR(16) NOT NULL,
+  FechaCreacion DATE NOT NULL,
+  Saldo MONEY NOT NULL,
+
+  ID_TDoc INT NOT NULL,
+  ID_TCuenta INT NOT NULL,
+  FOREIGN KEY (ID_TDoc) REFERENCES TipoDocIdentidad(ID_TDoc),
+  FOREIGN KEY (ID_TCuenta) REFERENCES TipoCuentaAhorro(ID_TCuenta)
 );
 
 CREATE TABLE Beneficiario
 (
-  ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  Porcentaje INT NOT NULL
+  ID_Beneficiario INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  NumeroCuenta VARCHAR(16) NOT NULL,
+  ValorDocIdentidad VARCHAR(16) NOT NULL,
+  Porcentaje INT NOT NULL,
+
+  ID_Parentezco INT NOT NULL,
+  FOREIGN KEY (ID_Parentezco) REFERENCES Parentezcos(ID_Parentezco)
 );
 
-CREATE TABLE Parentezco
+CREATE TABLE Beneficiario_Cuenta
 (
-  ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  Nombre VARCHAR(64) NOT NULL
+  ID_BC INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  
+  ID_Beneficiario INT NOT NULL,
+  ID_Cuenta INT NOT NULL,
+  FOREIGN KEY (ID_Beneficiario) REFERENCES Beneficiario(ID_Beneficiario),
+  FOREIGN KEY (ID_Cuenta) REFERENCES Cuenta(ID_Cuenta)
 );
 
 CREATE TABLE Usuario
 (
-  ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-  Nombre VARCHAR(64) NOT NULL
+  ID_Usuario INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Username VARCHAR(16) NOT NULL,
+  Pass VARCHAR(16) NOT NULL,
+  ValorDocIdentidad VARCHAR(16) NOT NULL,
+  EsAdministrador INT NOT NULL
+);
+
+CREATE TABLE Usuarios_Ver
+(
+  ID_UsuarioVer INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Username VARCHAR(16) NOT NULL,
+  NumeroCuenta VARCHAR(16) NOT NULL
 );
