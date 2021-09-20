c51e6adf2afdf39368600bd2170d92019d5fa973
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2021-09-20 04:40:58 -0600

    Update StoreProcedures.sql

--- Database/StoreProcedures.sql
+++ Database/StoreProcedures.sql
@@ -331,7 +331,7 @@ GO
 /* 
 Procedimiento SumarPorcentajes
 Objetivo: Dar a saber la suma de porcentajes 
-	Entradas : Numero de Cuenta
+	Entradas : Numero de Cuenta, Porcentaje y Valor de la persona a cambiar
 	Salidas  : Suma total de los datos a sumar
 */
 IF OBJECT_ID('SumarPorcentajes') IS NOT NULL
@@ -340,12 +340,14 @@ DROP PROC SumarPorcentajes
 END
 GO
 CREATE PROCEDURE SumarPorcentajes
-	 @inNumeroCuenta INT
+	 @inNumeroCuenta INT,
+	 @inPorcentaje INT,
+	 @inValorDocIndentidad INT
 AS
 BEGIN TRY 
-	SELECT SUM(B.Porcentaje)
+	SELECT SUM(B.Porcentaje + @inPorcentaje)
 	FROM Beneficiario B
-	WHERE (B.IDNumeroCuenta = @inNumeroCuenta)
+	WHERE (B.IDNumeroCuenta = @inNumeroCuenta AND B.IDValorDocIdentidad != @inValorDocIndentidad)
 
 END TRY
 BEGIN CATCH
